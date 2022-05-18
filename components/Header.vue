943da10ac2d263b0a158198d5bb880be923b72f3
ledieuelephant <contact.raphaelenlive@gmail.com>
2022-05-18 17:34:34 +0200

    Update of search system

--- components/Header.vue
+++ components/Header.vue
@@ -26,14 +26,19 @@
                 <hr class="mb-3">
                 <p class="text-center">Projets</p>
                 {{/* Projets */}}
-                <div class="flex justify-between" v-for="project in projectlist" :key="project.id">
-                    <a class="mt-2 inline-flex bg-blue-900 w-11/12 h-7 rounded-full" v-bind:href="'/'+project.id" >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mt-1.5 ml-2 bi bi-archive" viewBox="0 0 16 16">
-                            <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1V2zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5H2zm13-3H1v2h14V2zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
-                        </svg>
-                        <p class="pl-3 mt-0.5">{{project.name}}</p>
-                    </a>
-                    <div @click="deleteProject(project.id)" class="mt-2">X</div>
+                <div v-if="projectlist.length === 0">
+                    <h4 class="text-center mt-3">Aucun projet n'as été trouvé.</h4>
+                </div>
+                <div v-else>
+                    <div class="flex justify-between" v-for="project in projectlist" :key="project.id">
+                        <a class="mt-2 inline-flex bg-blue-900 w-11/12 h-7 rounded-full" v-bind:href="'/'+project.id" >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mt-1.5 ml-2 bi bi-archive" viewBox="0 0 16 16">
+                                <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1V2zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5H2zm13-3H1v2h14V2zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
+                            </svg>
+                            <p class="pl-3 mt-0.5">{{project.name}}</p>
+                        </a>
+                        <div @click="deleteProject(project.id)" class="mt-2">X</div>
+                    </div>
                 </div>
             </div>
         </div>
